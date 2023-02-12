36d36e2942d23c86394f479c2456787924cadb4f
GstvZenit <gus.marbet@gmail.com>
2023-02-11 20:58:29 -0400

    Agregado repositorios y datos por defecto

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,11 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,6 +13,27 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+//adding scope.e
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try 
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        //datos semilla:
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "Ocurrio un error durante la migracion");
+    }
+}
+//app.Run();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -5,10 +5,21 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string? Name { get; set; }
+        public string? Description { get; set;}
+        public decimal Price { get; set; }
+        public string? PictureUrl { get; set; }
+        public string? Ingredients { get; set; }
+        public ProductCategorie ProductCategorie { get; set; }
+        public int ProductCategorieId { get; set; }
+
+        public ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+
+
 
     }
 }
